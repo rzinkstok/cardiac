149ff45788b93ba78e08da0ee6108780569a3916
Roel Zinkstok <rzinkstok@gmail.com>
2019-01-06 12:26:39 +0100

    Redesign of debugging

--- cardiac.py
+++ cardiac.py
@@ -1,6 +1,8 @@
 import inspect
+from contextlib import contextmanager
 
 
+# Exception classes
 class MemoryOutOfRange(Exception):
     pass
 
@@ -25,8 +27,10 @@ class InvalidOperation(Exception):
     pass
 
 
+# CARDIAC part classes
 class Memory(object):
-    def init_mem(self, size=100):
+    def __init__(self, size=100):
+        super(Memory, self).__init__()
         self.mem_size = size
         self.mem_cells = ["" for i in range(self.mem_size)]
         self.mem_set(0, "001")
@@ -63,45 +67,9 @@ class Memory(object):
         return f"{'-'*int(data<0)}{abs(data):03}"
 
 
-class CPU(object):
-    def __init__(self):
-        print("Init CPU")
-        self.init_io()
-        self.init_mem()
-        self.reset()
-        self.opcodes = [
-            int(x[0][7:])
-            for x in inspect.getmembers(self.__class__)
-            if x[0].startswith("opcode_")
-        ]
-
-    def reset(self):
-        self.acc = 0
-        self.pc = 0
-        self.ir = 0
-        self.running = False
-
-    def fetch(self):
-        self.ir = self.mem_get_int(self.pc)
-        self.pc += 1
-
-    def process(self):
-        self.fetch()
-        opcode, arg = self.ir // 100, self.ir % 100
-        if opcode not in self.opcodes:
-            raise InvalidOperation(f"Opcode {opcode} not available")
-        op = getattr(self, f"opcode_{opcode}")
-        op(arg)
-
-    def run(self):
-        self.running = True
-        while self.running:
-            self.process()
-            # input()
-
-
 class IO(object):
-    def init_io(self):
+    def __init__(self):
+        super(IO, self).__init__()
         self.input_stack = []
         self.output_stack = []
 
@@ -131,99 +99,141 @@ class IO(object):
             raise InputExhausted("End of input")
 
 
+class CPU(object):
+    def __init__(self):
+        super(CPU, self).__init__()
+        self.reset()
+
+    def gather_instructions(self):
+        self.opcodes = {}
+        for x in inspect.getmembers(self.__class__):
+            if x[0].startswith("opcode_"):
+                parts = x[0].split("_")
+                self.opcodes[int(parts[1])] = parts[2]
+
+    def reset(self):
+        self.acc = 0
+        self.pc = 0
+        self.ir = 0
+        self.running = False
+
+    def fetch(self):
+        self.ir = self.mem_get_int(self.pc)
+        self.pc += 1
+
+    def process(self):
+        self.fetch()
+        opcode, arg = self.ir // 100, self.ir % 100
+        if opcode not in self.opcodes:
+            raise InvalidOperation(f"Opcode {opcode} not available")
+        opname = f"opcode_{opcode}"
+        if self.opcodes[opcode]:
+            opname += f"_{self.opcodes[opcode]}"
+        op = getattr(self, opname)
+        op(arg)
+
+
+# Main CARDIAC class
 class Cardiac(Memory, IO, CPU):
-    def debug(self, opcode, arg):
-        opcodes = {
-            0: "INP",
-            1: "CLA",
-            2: "ADD",
-            3: "TAC",
-            4: "SFT",
-            5: "OUT",
-            6: "STO",
-            7: "SUB",
-            8: "JMP",
-            9: "HRS",
-        }
-        print()
-        print("-" * 60)
-        print(f"{opcode} {opcodes[opcode]} [{arg:02}]")
-        print("-" * 60)
+    def __init__(self, verbose=False):
+        super(Cardiac, self).__init__()
+        self.opcodes = {}
+        self.gather_instructions()
+        self.verbose = verbose
 
-    def debug_mem(self, display=100):
-        print()
-        print(f"ACC: {self.acc:04}")
-        print(f"PC:  {self.pc:02}")
-        print()
-        print(f"MEM:")
-        print("".join([f"{i:02}  " for i in range(display)]))
-        print("".join([f"{x} " if x else "    " for x in self.mem_cells[:display]]))
-        print()
-        try:
-            print(f"IN: {self.input_stack[-1]}")
-        except IndexError:
-            print("IN: None")
-        print("OUT:")
-        print(self.output_stack)
+    def debug(func):
+        opcode = int(func.__name__.split("_")[1])
 
-    def opcode_0(self, arg):
-        self.debug(0, arg)
+        def debug_wrapper(self, arg):
+            if self.verbose:
+                print()
+                print("-" * 60)
+                print(f"{opcode} {self.opcodes[opcode].upper()} [{arg:02}]")
+                print("-" * 60)
+
+            res = func(self, arg)
+
+            if self.verbose:
+                print()
+                print(f"ACC: {self.acc:04}")
+                print(f"PC:  {self.pc:02}")
+                print()
+                print(f"MEM:")
+                print("".join([f"{i:02}  " for i in range(self.mem_size)]))
+                print(
+                    "".join(
+                        [
+                            f"{x} " if x else "    "
+                            for x in self.mem_cells[: self.mem_size]
+                        ]
+                    )
+                )
+                print()
+                try:
+                    print(f"NEXT IN: {self.input_stack[-1]}")
+                except IndexError:
+                    print("NEXT IN: None")
+                print("OUT:")
+                print(self.output_stack)
+
+            return res
+
+        return debug_wrapper
+
+    @debug
+    def opcode_0_inp(self, arg):
         inp = self.get_input()
         self.mem_set(arg, inp)
-        self.debug_mem()
 
-    def opcode_1(self, arg):
-        self.debug(1, arg)
+    @debug
+    def opcode_1_cla(self, arg):
         self.acc = self.mem_get_int(arg)
-        self.debug_mem()
 
-    def opcode_2(self, arg):
-        self.debug(2, arg)
+    @debug
+    def opcode_2_add(self, arg):
         self.acc += self.mem_get_int(arg)
-        self.debug_mem()
 
-    def opcode_3(self, arg):
-        self.debug(3, arg)
+    @debug
+    def opcode_3_tac(self, arg):
         if self.acc < 0:
             self.pc = arg
-        self.debug_mem()
 
-    def opcode_4(self, arg):
-        self.debug(4, arg)
+    @debug
+    def opcode_4_sft(self, arg):
         l, r = arg // 10, arg % 10
         self.acc = (self.acc * pow(10, l) / pow(10, r)) % 10000
 
-    def opcode_5(self, arg):
-        self.debug(5, arg)
+    @debug
+    def opcode_5_out(self, arg):
         self.put_output(self.mem_get(arg))
-        self.debug_mem()
 
-    def opcode_6(self, arg):
-        self.debug(6, arg)
+    @debug
+    def opcode_6_sto(self, arg):
         self.mem_set(arg, self.acc)
-        self.debug_mem()
 
-    def opcode_7(self, arg):
-        self.debug(7, arg)
+    @debug
+    def opcode_7_sub(self, arg):
         self.acc -= self.mem_get_int(arg)
-        self.debug_mem()
 
-    def opcode_8(self, arg):
-        self.debug(8, arg)
+    @debug
+    def opcode_8_jmp(self, arg):
         self.mem_set(99, 800 + self.pc)
         self.pc = arg
-        self.debug_mem()
 
-    def opcode_9(self, arg):
-        self.debug(9, arg)
+    @debug
+    def opcode_9_hrs(self, arg):
         self.reset()
         self.pc = arg
-        self.debug_mem()
+
+    def run(self):
+        self.running = True
+        while self.running:
+            self.process()
+            # input()
+        self.format_output()
 
 
 if __name__ == "__main__":
-    c = Cardiac()
+    c = Cardiac(verbose=False)
     c.read_deck("deck5.txt")
-    c.debug_mem()
     c.run()
-    c.format_output()
